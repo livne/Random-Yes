a92640559c698e0094c6021b100227a4d56138b4
Shahar Livne <shahar@randomyes.com>
2009-05-16 20:52:44 +0200

    add auth_backend for CustomUser(User) + UUIDField myview for test

--- app/models.py
+++ app/models.py
@@ -1,2 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User, UserManager
+import uuid
+
+class UUIDField(models.CharField) :
+    
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = kwargs.get('max_length', 64 )
+        kwargs['blank'] = True
+        models.CharField.__init__(self, *args, **kwargs)
+    
+    def pre_save(self, model_instance, add):
+        if add :
+            value = str(uuid.uuid4())
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super(models.CharField, self).pre_save(model_instance, add)
+
+
+
+class CustomUser(User):
+    """User with app settings."""
+    uuid = UUIDField(primary_key=True, editable=False)
+
+    # Use UserManager to get the create_user method, etc.
+    objects = UserManager()
+
 
--- app/views.py
+++ app/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.views import login, logout
+
+def myview(request):
+    if request.user.is_authenticated():
+      return HttpResponseRedirect('/loggedin/')
+    else:
+      if request.POST:
+        authd = login(request)
+        authd = request.user.is_authenticated()
+        if authd:
+          return HttpResponseRedirect('/loggedin/')
+      logged_in = login(request)
+      return logged_in
