cff8714e4ce5e9d52079301f346a8e8eafee5519
Shahar Livne <shahar@randomyes.com>
2009-05-20 13:08:29 +0200

    add new user generator (visit go)

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^status/$', 'randomyes.app.views.status'),
     (r'^inbox/(?P<token>[0-9,a-z,A-Z]{30})/$', 'randomyes.app.views.inbox'),
+    (r'^$', 'randomyes.app.views.new'),
 )
 
--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 from app.auth_backends import CustomUserModelBackend 
 from django import template
 from django.utils.translation import ugettext_lazy as _
 from app.models import CustomUser
 from django.contrib.auth import authenticate, login
-
+from string import letters, digits
+from random import choice
 
 def status(request):
     if request.user.is_authenticated():
@@ -19,3 +21,8 @@ def inbox(request, token):
         if user.is_active:
             login(request, user)
     return render_to_response('app/welcome.html', {'error_message': token}, context_instance=template.RequestContext(request))
+
+def new(request):
+    token=''.join(choice(letters+digits) for i in xrange(30))
+    return HttpResponseRedirect('/go/inbox/%s' % token)
+
