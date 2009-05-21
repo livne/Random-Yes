e6ee89462ddd047f7ae10c9993c4db00ca86159d
Shahar Livne <shahar@randomyes.com>
2009-05-21 18:00:31 +0200

    add greetings to base.html and logout

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^status/$', 'randomyes.app.views.status'),
     (r'^login/(?P<token>[0-9,a-z,A-Z]{30})/$', 'randomyes.app.views.rylogin'),
+    (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'app/logout.html'}),
     (r'^new/$', 'randomyes.app.views.new'),
 )
 
